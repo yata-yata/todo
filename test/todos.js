b4d214e5c15059defb70018de4bb15d9b7db189c
Mike Davis <mike@michaeljacobdavis.com>
2014-05-03 22:13:25 -0700

    Started unit tests

---
+++ test/todos.js
@@ -0,0 +1,85 @@
+// Load modules
+var Lab = require('lab'),
+    Hapi = require('hapi'),
+    Todos = require('../lib/services/todos'),
+
+    // Declare internals
+    internals = {},
+
+    // Test aliases
+    expect = Lab.expect,
+    before = Lab.before,
+    beforeEach = Lab.beforeEach,
+    after = Lab.after,
+    afterEach = Lab.afterEach,
+    describe = Lab.experiment,
+    it = Lab.test,
+    assert = Lab.assert;
+
+internals.prepareServer = function(callback){
+  var server = new Hapi.Server({ labels: ['api'] });
+  server.pack.require('../', {}, function(err){
+    expect(err).to.not.exist;
+    callback(server);
+  });
+};
+
+describe('Todos', function(){
+
+  describe('GET', function(){
+    var orig;
+
+    beforeEach(function(done){
+      orig = Todos.prototype.get;
+      done();
+    });
+
+    afterEach(function(done){
+      Todos.prototype.get = orig;
+      done();
+    });
+
+    it('should return an array of todos if no ID is passed', function(done){
+
+      // Given
+      Todos.prototype.get = function(options, callback){
+        callback(null, [1,2,3]);
+      };
+
+      internals.prepareServer(function(server){
+
+        // When
+        server.inject({ method: 'GET', url: '/todos' }, function(response){
+
+          // Then
+          expect(response.result).to.be.instanceof(Array);
+
+          done();
+        });
+      });
+    });
+
+    it('should return a todo object if an id is passed', function(done){
+
+      // Given
+      Todos.prototype.get = function(options, callback){
+        callback(null, {
+          title: 'todo'
+        });
+      };
+
+      internals.prepareServer(function(server){
+
+        // When
+        server.inject({ method: 'GET', url: '/todos' }, function(response){
+
+          // Then
+          expect(response.result).to.be.an('object');
+
+          done();
+        });
+      });
+    });
+
+  });
+});
